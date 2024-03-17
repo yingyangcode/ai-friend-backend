500a3b25f8ecef68c799fdfc70cfc7c6eeebd42d
Marcel Anis <marcel.anis.21@gmail.com>
2024-03-17 13:17:21 -0400

    api endpoint for chat completion

---
+++ index.js
@@ -0,0 +1,138 @@
+import { exec } from "child_process";
+import cors from "cors";
+import dotenv from "dotenv";
+import voice from "elevenlabs-node";
+import express from "express";
+import { promises as fs } from "fs";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "-", // Your OpenAI API key here, I used "-" to avoid errors when the key is not set but you should not do that
+});
+
+const elevenLabsApiKey = process.env.ELEVEN_LABS_API_KEY;
+const voiceID = process.env.ELEVEN_LABS_VOICE_ID;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/voices", async (req, res) => {
+  res.send(await voice.getVoices(elevenLabsApiKey));
+});
+
+const execCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) reject(error);
+      resolve(stdout);
+    });
+  });
+};
+
+const lipSyncMessage = async (message) => {
+  const time = new Date().getTime();
+  console.log(`Starting conversion for message ${message}`);
+  await execCommand(
+    `ffmpeg -y -i ./audios/message_${message}.mp3 ./audios/message_${message}.wav`
+    // -y to overwrite the file
+  );
+  console.log(`Conversion done in ${new Date().getTime() - time}ms`);
+  await execCommand(
+    `./bin/rhubarb -f json -o ./audios/message_${message}.json ./audios/message_${message}.wav -r phonetic`
+  );
+  // -r phonetic is faster but less accurate
+  console.log(`Lip sync done in ${new Date().getTime() - time}ms`);
+};
+
+app.post("/chat", async (req, res) => {
+  const userMessage = req.body.message;
+  console.log("userMessage", userMessage);
+  if (!userMessage) {
+    res.send({
+      messages: [
+        {
+          text: "Hey man! How's it going?",
+          audio: await audioFileToBase64("audios/intro_0.wav"),
+          lipsync: await readJsonTranscript("audios/intro_0.json"),
+          facialExpression: "smile",
+          animation: "Talking_1",
+        },
+        {
+          text: "Dude, you vanished off the face of the earth or what? Let's not go ages without hanging out, alright?",
+          audio: await audioFileToBase64("audios/intro_1.wav"),
+          lipsync: await readJsonTranscript("audios/intro_1.json"),
+          facialExpression: "sad",
+          animation: "Crying",
+        },
+      ],
+    });
+    return;
+  }
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-1106",
+    max_tokens: 1000,
+    temperature: 0.6,
+    response_format: {
+      type: "json_object",
+    },
+    messages: [
+      {
+        role: "system",
+        content: `
+        You are a virtual guy best friend.
+        You will always reply with a JSON array of messages. With a maximum of 3 messages.
+        Each message has a text, facialExpression, and animation property.
+        The different facial expressions are: smile, sad, angry, surprised, funnyFace, and default.
+        The different animations are: Talking_0, Talking_1, Talking_3, Crying, Laughing, Salsa, Idle, Greeting, and Angry. 
+        `,
+      },
+      {
+        role: "user",
+        content: userMessage || "Hello",
+      },
+    ],
+  });
+  let messages = await JSON.parse(completion.choices[0].message.content);
+  if (messages.messages) {
+    messages = messages.messages; // ChatGPT is not 100% reliable, sometimes it directly returns an array and sometimes a JSON object with a messages property
+  }
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    // generate audio file
+    const fileName = `./audios/message_${i}.mp3`; // The name of your audio file
+
+    const textInput = message.text; // The text you wish to convert to speech
+
+    await voice.textToSpeech(elevenLabsApiKey, voiceID, fileName, textInput);
+
+    // generate lipsync
+    await lipSyncMessage(i);
+    message.audio = await audioFileToBase64(fileName);
+    message.lipsync = await readJsonTranscript(`./audios/message_${i}.json`);
+  }
+
+  res.send({ messages });
+});
+
+const readJsonTranscript = async (file) => {
+  const data = await fs.readFile(file, "utf8");
+  return JSON.parse(data);
+};
+
+const audioFileToBase64 = async (file) => {
+  const data = await fs.readFile(file);
+  return data.toString("base64");
+};
+
+app.listen(port, () => {
+  console.log(`Virtual Friend listening on port ${port}`);
+});
